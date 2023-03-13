ddd3ea962ff1ec0cafed9edcf90945a293384c34
Patrycja Galant <patrycja.ciruk@gmail.com>
2023-03-13 13:33:56 +0100

    Implement Array Deep Count kata

---
+++ CodewarsSolutions/Array Deep Count/ArrayDeepCount.cs
@@ -0,0 +1,27 @@
+﻿namespace CodeWars;
+
+public class ArrayDeepCount
+{
+    // You are given an array. Complete the function that returns the number of ALL elements within an array, including any nested arrays.
+    public static int DeepCount(object a)
+    {
+        int counter = 0;
+        return CountHelper(a, counter);
+    }
+    private static int CountHelper(object a, int counter)
+    {
+        if (a.GetType().IsArray)
+        {
+            object[] array = (object[])a;
+            foreach (object obj in array)
+            {
+                if (obj.GetType().IsArray)
+                {
+                    counter = CountHelper(obj, counter);
+                }
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
