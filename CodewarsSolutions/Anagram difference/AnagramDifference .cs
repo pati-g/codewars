b26b96ba025623d5fc474d42279a5e8e6540958a
Patrycja Galant <patrycja.ciruk@gmail.com>
2023-03-13 13:14:42 +0100

    Implement Anagram Difference kata

---
+++ CodewarsSolutions/Anagram difference/AnagramDifference .cs
@@ -0,0 +1,40 @@
+﻿internal class AnagramDifference
+{
+    // Calculate how many letters have to be removed from each of 2 words to make them anagrams
+    public static int CalculateAnagramDifference(string word1, string word2)
+    {
+        var dictionary = new Dictionary<char, int>();
+        foreach (char c in word1)
+        {
+            if (dictionary.ContainsKey(c))
+                dictionary[c]++;
+            else
+                dictionary.Add(c, 1);
+        }
+
+        foreach (var c in word2)
+        {
+            if (dictionary.ContainsKey(c))
+            {
+                dictionary[c]--;
+                if (dictionary[c] == 0)
+                {
+                    dictionary.Remove(c);
+                }
+            }
+            else
+            {
+                if (dictionary.ContainsKey(c))
+                {
+                    dictionary[c]++;
+                }
+                else
+                {
+                    dictionary.Add(c, 1);
+                }
+            }
+        }
+        int count = dictionary.Values.Sum();
+        return count;
+    }
+}
